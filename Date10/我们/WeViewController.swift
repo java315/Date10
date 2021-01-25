7f58e13eecfd6ab5e9fbe50b0138cd8aecffaed4
zb-nju <181860106@smail.nju.edu.cn>
2021-01-26 00:08:28 +0800

    画了前三个主界面

---
+++ Date10/我们/WeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WeViewController.swift
+//  Date10
+//
+//  Created by zb-nju on 2021/1/25.
+//
+
+import UIKit
+
+class WeViewController: UIViewController {
+    @IBOutlet weak var boyAvatar: UIImageView!
+    @IBOutlet weak var boyName: UILabel!
+    @IBOutlet weak var girlAvatar: UIImageView!
+    @IBOutlet weak var girlName: UILabel!
+    @IBOutlet weak var togetherTimeLabel: UILabel!
+    @IBOutlet weak var sweetValueLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Date10/消息/MessageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessageViewController.swift
+//  Date10
+//
+//  Created by zb-nju on 2021/1/25.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
