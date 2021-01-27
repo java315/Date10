c56c87068f29fca4daa9539595f517f3b8df1a3a
icetea <42881835+youngstudent2@users.noreply.github.com>
2021-01-26 22:49:27 -0800

    bug fix

--- Date10/Data/GlobalData.swift
+++ Date10/Data/GlobalData.swift
@@ -24,10 +24,12 @@ class GlobalData {
     
     private init() {
         let defaults = UserDefaults.standard
-//        guard defaults.bool(forKey: defaultsKeys.NotFirstLogin) else {
-//            self.firstLogin = true
-//            defaults.set(false, forKey: defaultsKeys.NotFirstLogin)
-//        }
+        let f = defaults.bool(forKey: defaultsKeys.NotFirstLogin)
+        print("flag:",f)
+        if !defaults.bool(forKey: defaultsKeys.NotFirstLogin) {
+            self.firstLogin = true
+            defaults.set(false, forKey: defaultsKeys.NotFirstLogin)
+        }
         
         initUsers()
         initCouples()
