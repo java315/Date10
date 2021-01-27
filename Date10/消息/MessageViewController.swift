96004dea2f0eb64d487a8d6444eb4d8f5b14b092
icetea <42881835+youngstudent2@users.noreply.github.com>
2021-01-27 11:19:18 -0800

    do Message View

--- Date10/消息/MessageViewController.swift
+++ Date10/消息/MessageViewController.swift
@@ -8,16 +8,21 @@
 import UIKit
 
 class MessageViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var messageCollection: UICollectionView!
     var communications = [Communication]()
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCommunications()
-        // Do any additional setup after loading the view.
+        messageCollection.delegate = self
+        messageCollection.dataSource = self
+        
     }
     
     private func loadCommunications() {
         let globalData = GlobalData.getInstance()
-        self.communications = globalData.getAllCommunications()
+        self.communications += globalData.getAllCommunications()
+        print("communication count \(communications.count)")
     }
     /*
     // MARK: - Navigation
@@ -31,18 +36,20 @@ class MessageViewController: UIViewController {
 
 }
 
-extension MessageViewController : UICollectionViewDataSource {
+extension MessageViewController : UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return communications.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCollectionViewCell", for: indexPath) as! MessageCollectionViewCell
         let m = communications[indexPath.row].getLeastMessage()
         cell.message.text = m?.content
         cell.time.text = DateUtil.ZoneTime(date: (m?.sendTime)!, dateFormat: DateUtil.FullTimeFormat)
         cell.senderName.text = m?.sender
         cell.senderAvatar.image = UIImage(named: "user")
+        print(cell.message.text)
         return cell
     }
     
